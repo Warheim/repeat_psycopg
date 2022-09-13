7875752f92f12df4710c23640c524c8f8e72cad0
Steshin <warheim24@gmail.com>
2022-09-13 17:08:06 +0300

    Connection added

--- homework.py
+++ homework.py
@@ -2,4 +2,10 @@ import psycopg2
 import configparser
 
 if __name__ == '__main__':
-    pass
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+    conn = psycopg2.connect(database=config['DB']['name'], user=config['DB']['user'], password=config['DB']['password'])
+    with conn.cursor() as cur:
+        pass
+        conn.commit()
+    conn.close()

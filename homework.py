2cf53c86e96177b3b6f133a6c64819523d0e306f
Steshin <warheim24@gmail.com>
2022-09-14 15:30:28 +0300

    optimizing the search

--- homework.py
+++ homework.py
@@ -89,6 +89,68 @@ def update_info(cur, customer_id, first_name=None, last_name=None, email=None, o
     conn.commit()
 
 
+def delete_phone_number(cur, phone_number):
+    cur.execute("""
+    DELETE FROM Phonebook
+    WHERE phone_number = %s RETURNING id;
+    """, (phone_number,))
+    if not cur.fetchone():
+        print('There is no number to delete like', phone_number)
+    conn.commit()
+
+
+def delete_customer(cur, customer_id):
+    cur.execute("""
+    DELETE FROM Phonebook
+    WHERE customer_id = %s RETURNING customer_id;
+    DELETE FROM Customer
+    WHERE id = %s RETURNING id;
+    """, (customer_id, customer_id))
+    if not cur.fetchone():
+        print('There is no customer with id =', customer_id)
+    conn.commit()
+
+
+def find_customer(cur, first_name=None, last_name=None, email=None, phone_number=None):
+    cur.execute("""
+    SELECT * FROM Customer c
+    JOIN Phonebook p on c.id = p.customer_id
+    WHERE email = %s;
+    """, (email,))
+    if not cur.fetchall():
+        cur.execute("""
+        SELECT * FROM Customer c
+        JOIN Phonebook p on c.id = p.customer_id
+        WHERE phone_number = %s;
+        """, (phone_number,))
+        if not cur.fetchall():
+            cur.execute("""
+            SELECT * FROM Customer c
+            JOIN Phonebook p on c.id = p.customer_id
+            WHERE first_name = %s AND last_name = %s;
+            """, (first_name, last_name))
+            if not cur.fetchall():
+                cur.execute("""
+                SELECT * FROM Customer c
+                JOIN Phonebook p on c.id = p.customer_id
+                WHERE last_name = %s;
+                """, (last_name,))
+                if not cur.fetchall():
+                    cur.execute("""
+                    SELECT * FROM Customer c
+                    JOIN Phonebook p on c.id = p.customer_id
+                    WHERE first_name = %s;
+                    """, (first_name,))
+                else:
+                    print(cur.fetchall())
+            else:
+                print(cur.fetchall())
+        else:
+            print(cur.fetchall())
+    else:
+        print(cur.fetchall())
+
+
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('settings.ini')
@@ -101,5 +163,10 @@ if __name__ == '__main__':
         add_phone(cursor, 1, '123-45-67')
         add_phone(cursor, 1, '777-9-777')
         add_phone(cursor, 2, '987412365')
-        update_info(cursor, 1, 'Changed', 'All', 'info@all', '123-45-67', '555-OK')
+        # update_info(cursor, 1, 'Changed', 'All', 'info@all', '123-45-67', '555-OK')
+        # update_info(cursor, 3, 'Fail', 'Fail', 'mustbe@fail')
+        # update_info(cursor, 2, 'And', 'You', None, '987412365', None)
+        # delete_phone_number(cursor, '987412365')
+        # delete_customer(cursor, 2)
+        find_customer(cursor, 'John', 'Doe', 'somebody@buddy.com')
     conn.close()
